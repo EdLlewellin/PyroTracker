2edfae8e2906cd65600b3bff411351f6ceb653f0
LLEWELLIN <dgl0el@durham.ac.uk>
2025-05-05 17:09:09 +0100

    Added pdf user manual, embedded icon, and updated release name in config.py

--- config.py
+++ config.py
@@ -89,4 +89,4 @@ MAX_ABS_SCALE = 50.0    # Absolute maximum allowed view scale factor (e.g., 50x 
 # --- Application Info ---
 APP_NAME = "PyroTracker"
 APP_ORGANIZATION = "Durham University"
-APP_VERSION = "2.1 Coords"
+APP_VERSION = "2.0.0 Beta"
